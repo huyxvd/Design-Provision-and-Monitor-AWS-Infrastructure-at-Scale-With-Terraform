803b06a06c91eece0126d3186fa8d49ff190a1a6
Huy Đẹp Trai <huyxvd@gmail.com>
2023-03-04 14:05:10 +0700

    review code

--- Task 5/Part2/outputs.tf
+++ Task 5/Part2/outputs.tf
@@ -1,5 +1,5 @@
 # TODO: Define the output variable for the lambda function.
 output "aws_lambda_arn" {
-  description = "ID of the EC2 instance"
+  description = "this will return lambda arn after provision"
   value       = aws_lambda_function.greet_lambda.arn
 }
